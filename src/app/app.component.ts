33e3b8fd56956ef0be86b1dc7ffc0cbc5c8ee1c5
alvalle09 <phredvalle@gmail.com>
2020-11-04 23:39:11 -0600

    use pure:false for pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,12 +39,24 @@ export class AppComponent {
     },
 
   ];
+
   filteredStatus = '';
+
   getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
     return {
       'list-group-item-success': server.status === 'stable',
       'list-group-item-warning': server.status === 'offline',
       'list-group-item-danger': server.status === 'critical'
     };
+  }
+
+  onAddServer() {
+    this.servers.push({
+      instanceType: 'small',
+      name: 'New Server', 
+      status: 'critical',
+      started: new Date(4, 11, 2020)
+    });
+
   }
 }
